00dad32eefdc40be1d1c1f48e1a71fd9f3c7e38d
sjaiswal-cmz <sjaiswal@cloudamize.com>
2026-06-11 00:37:25 +0530

    new changes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -31,6 +31,10 @@ func main() {
 	feedbackHandler := handlers.NewFeedbackHandler(sheetsStore)
 
 	mux := http.NewServeMux()
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
 	mux.HandleFunc("POST /feedback", feedbackHandler.SubmitFeedback)
 
 	srv := &http.Server{
